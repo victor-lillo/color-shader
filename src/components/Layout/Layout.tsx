5cf31dca1cb92e42e6dd3d66eee9be8e3a4fa53c
fentosv <fentos.dev@gmail.com>
2022-11-03 17:20:51 +0100

    Layout component structure fixed

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,11 @@
 import Navbar from '@components/Navbar'
 import Footer from '@components/Footer'
-
+import styles from './Layout.module.scss'
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Navbar />
-      <main className='body'>{children}</main>
+      <main className={styles.body}>{children}</main>
       <Footer />
     </>
   )
