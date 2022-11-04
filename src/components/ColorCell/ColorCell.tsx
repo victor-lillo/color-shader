9d73f1ef87c4581d31fe6b42aa3862d5aec0a3cd
fentosv <fentos.dev@gmail.com>
2022-11-04 10:56:29 +0100

    Fixed "copy to copied" animation

--- src/components/ColorCell/ColorCell.tsx
+++ src/components/ColorCell/ColorCell.tsx
@@ -3,17 +3,9 @@ import { useState } from 'react'
 import styles from './ColorCell.module.scss'
 const ColorCell = ({ color, percent }: { color: string; percent?: string }) => {
   const [clicked, setClicked] = useState<boolean>(false)
-  const [isHovering, setIsHovering] = useState(false)
 
-  const timeToDissapear = 2000
+  const timeToDissapear = 2400
 
-  const handleMouseOver = () => {
-    setIsHovering(true)
-  }
-
-  const handleMouseOut = () => {
-    setIsHovering(false)
-  }
   const handleClick = () => {
     navigator.clipboard.writeText(color)
     setClicked(true)
@@ -26,11 +18,11 @@ const ColorCell = ({ color, percent }: { color: string; percent?: string }) => {
     <div className={styles.container}>
       {percent && <p className={styles.text}>{`${percent}%`}</p>}
       <div
-        className={classnames(styles.cell, { [styles['cell--copied']]: clicked, [styles['cell--copy']]: isHovering })}
+        className={classnames(styles.cell, {
+          [styles['cell--copied']]: clicked,
+        })}
         style={{ '--content': `${color}`, '--delay': `${timeToDissapear / 1000}s` } as React.CSSProperties}
         onClick={handleClick}
-        onMouseOver={handleMouseOver}
-        onMouseOut={handleMouseOut}
       ></div>
       <p className={styles.text}>{color}</p>
     </div>
