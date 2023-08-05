e4de5e1d298a48cc93c17cbdda7609118ef633fb
fentosv <fentos.dev@gmail.com>
2023-08-05 11:27:40 +0200

    Add debounce timeout to constants

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,4 @@ export const COLOR_NAME = 'color'
 export const QUANTITY_NAME = 'quantity'
 export const PREFIX_NAME = 'prefix'
 export const CUSTOM_PROP_BASE_NAME = 'f-primary'
+export const DEBOUNCE_ROUTE_TIMEOUT = 300
